070bae0f2b1dc85fa4672b623e7c0270cf659e17
Brooke Ross <brooke.ross2800@gmail.com>
2023-03-09 04:03:58 -0700

    Light and Dark Mode CSS

--- client/src/NavBarFolder/NavBar.js
+++ client/src/NavBarFolder/NavBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './NavBar.css'; // Import CSS styles for the component
 import { NavLink } from 'react-router-dom';
+import darkmode from './iconImages/darkmode.png'
+import lightmode from './iconImages/lightmode.png'
 
 function NavBar({isDarkMode, setIsDarkMode}) {
 
@@ -11,7 +13,7 @@ function NavBar({isDarkMode, setIsDarkMode}) {
   return (
     <nav className={isDarkMode ? 'dark-mode' : 'light-mode'}>
       <button onClick={toggleDarkMode}>
-        {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+        <img src={isDarkMode ? lightmode : darkmode} alt="Toggle dark mode" />
       </button>
       <NavLink to="/" style={{marginRight: '20px'}} >Home</NavLink>
       <NavLink to="/about">About</NavLink>
